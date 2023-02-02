363be3999980142b47b12e882e1230531b42e655
CarloColletti~ <carlocolletti5@gmail.com>
2023-02-02 15:56:15 +0100

    aggiunto array con generatore delle bombe

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 /** 
 Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
-ed emetto un messaggio in console con il numero della cella cliccata.
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco 
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. 
+Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell'array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati
+ - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. 
+ Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti 
+(ovvero quando ha rivelato tutte le celle che non sono bombe).
 
+Errata corrige: rendere le caselle "non cliccabili" e far finire la partita è un superbonus, non è richiesto dalla consegna!
 
-Bonus
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha cliccato su una cella che non era una bomba.
+BONUS
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-Consigli del giorno: party_wizard:
+- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+Consigli del giorno: :party_wizard:
 Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 Ad esempio
-Di cosa ho bisogno per generare i numeri ?
-    Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+
+Di cosa ho bisogno per generare i numeri?
+Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 Le validazioni e i controlli possiamo farli anche in un secondo momento.
-Aggiungo uno screen senza e con bonus, ma potete realizzare la parte grafica a vostro gusto(dopo aver finito la parte logica!)
+
+SUPERBONUS 1
+Quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle.
+
+SUPERBONUS 2
+Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
 
 */
 
@@ -74,6 +86,22 @@ function createGrid(gridElement, dimension) {
     // mi assicuro che la griglia sia vuota
     gridElement.innerHTML = " ";
 
+    // creo un array vuoto che conterra le bombe 
+    const bombs = [];
+    // crea un numero random da 1 a il numero di caselle 
+    for (let i = 0; i < 16; i++) {
+        let randomNumber = getRandomNumber(1, dimension);
+
+        // se il numero non è contenuto nel array mettilo altrimenti riprova
+        while (bombs.includes(randomNumber)) {
+            randomNumber = getRandomNumber(1, dimension)
+        }
+
+        // aggiungi il numero all'arry 
+        bombs.push(randomNumber);
+        console.log(bombs);
+    }
+
     // per 100 volte (numero quadrati all'interno della griglia)
     for (let i = 0; i < dimension; i++) {
         // creo un div per far contenere le classi ecc 
@@ -107,4 +135,16 @@ function createGrid(gridElement, dimension) {
         squareElement.append(number);
 
     }
+}
+
+/**
+ *
+ * @param {int} min numero minimo da cui parte il ciclo
+ * @param {int} man numero massimo da cui parte il ciclo
+ *  
+*/
+
+// creo un array di numeri random che saranno le bombe \
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
